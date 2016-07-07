f3a3013f28c536e77e2e207087cf026a5e899410
thomas <tf@kumquats.fr>
2016-07-07 11:15:04 +0200

    - ajout tp5.1 regexp

---
+++ 5.1-regexp-form/demarrage/main.js
@@ -0,0 +1,8 @@
+document.querySelector( 'form' ).addEventListener( 'submit', submitHandler );
+
+function submitHandler( event )
+{
+	// on empèche le navigateur de recharger la page
+	event.preventDefault();
+
+}
