a91be88128c7ee6507315adf9c2c44bf1a6ef4eb
thomas <tf@kumquats.fr>
2019-06-10 16:45:50 +0200

    7.1 donnees ajax : demarrage (copie demarrage 6.1 regexp)

---
+++ 7.1-donnees-ajax/demarrage/src/SearchResults.js
@@ -0,0 +1,38 @@
+import Component from "./Component";
+
+class SearchResultRenderer extends Component {
+	result;
+
+	constructor( result ) {
+		super();
+		this.result = result;
+	}
+
+	render() {
+		const { show: { id, image, name, summary, officialSite, premiered } } = this.result;
+		return `<li>
+			<article class="searchResultRenderer">
+				${ image ? `<img src="${image.medium}" class="thumbnail" />` : ''}
+				<section class="infos">
+					<h3>${name}</h3>
+					<p>${summary}</p>
+					${ premiered ? `<time datetime="${premiered}">${premiered}</time>` : ''}
+					${ officialSite ? `<a href="${officialSite}">${officialSite}</a>` : ''}
+				</section>
+			</article>
+		</li>`
+	}
+}
+
+export default class SearchResults extends Component {
+	#results;
+
+	set results( value ) {
+		this.#results = value;
+		this.children = this.#results.map( result => new SearchResultRenderer( result ) );
+	}
+
+	constructor() {
+		super( 'ul' );
+	}
+}
