1753ce5f45223a49af0fefeeb1ffae626ebb7946
thomas <tf@kumquats.fr>
2019-06-14 01:13:25 +0200

    8.1 html5 video : corrige = demarrage

---
+++ 8.1-html5-video/corrige/server/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import SocketServer from 'socket.io';
+import http from 'http';
+import renderPage from './renderPage';
+
+// création des serveurs express et socket.io
+const app = express(),
+	server = http.createServer( app ),
+	io = new SocketServer( server ),
+	port = 8080;
+
+// serveur http express
+// pour la page index...
+app.get( '/', ( req, res ) => {
+	const html = renderPage();
+	res.send( html );
+} );
+// ... et pour tous les fichiers statiques qui se trouvent dans ./public
+app.use( express.static( './public' ) );
+
+// serveur websocket socket.io
+// pour les échanges de données temps réel
+const searchHistory = [];
+io.on( 'connection', socket => {
+	console.log( `Nouveau visiteur : ${socket.id}` );
+	// lorsqu'un nouvel utilisateur se connecte, on lui envoie l'historique complet des recherches précédentes
+	socket.emit( 'searchHistoryUpdate', searchHistory );
+	// lorsqu'un utilisateur fait une recherche...
+	socket.on( 'search', value => {
+		console.log( 'Nouvelle recherche lancée : ', value );
+		// ... on ajoute la recherche dans l'historique ...
+		searchHistory.unshift( {
+			search: value,
+			socketId: socket.id,
+			date: new Date(),
+		} );
+		// ... et on renvoie l'historique complet à tous les utilisateurs connectés
+		io.emit( 'searchHistoryUpdate', searchHistory );
+	} );
+} );
+
+server.listen( port, () => {
+	console.log( `l'appli JSTV est lancée sur le port ${port} !
+	connectez vous sur http://localhost${port != 80 ? port : ''}` );
+} );
---
+++ 8.1-html5-video/corrige/src/main.js
@@ -0,0 +1,91 @@
+import $ from 'jquery';
+import renderSlideshow, { getSlideshowImages, slideNext } from "./slideshow.js";
+import Component from "./Component.js";
+import SearchForm from "./SearchForm.js";
+import SearchResults from "./SearchResults.js";
+import LiveSearch from './LiveSearch.js';
+
+// codez ici votre TP
+console.log( 'Welcome to ', { title: 'JSTV', emoji: '📺' } );
+
+
+// on initialise le diaporama
+const slideshowContainer = $( '.slideshow' ),
+	images = getSlideshowImages( slideshowContainer );
+if ( images.length ) {
+	slideshowContainer.html( renderSlideshow( images ) );
+}
+// toutes les 2 secondes on fait défiler le diaporama
+setInterval( () => slideNext( slideshowContainer ), 2000 );
+// on ajoute la classe CSS 'single' au slideshow
+// pour n'afficher que la première image
+slideshowContainer.addClass( 'single' );
+
+// création du header
+const c = new Component( 'h1', [
+		'JS',
+		new Component( 'em', [ 'TV' ] )
+	],
+	{ class: 'logo' }
+);
+$( 'body > header' ).html( c.render() );
+
+// Formulaire de recherche
+const searchForm = new SearchForm();
+const searchFormContainer = $( '.searchForm' );
+searchFormContainer.html( searchForm.render() );
+searchForm.mount( searchFormContainer );
+// au submit du formulaire de recherche on exécute la fonction suivante
+searchForm.onSubmit = value => {
+	console.log( `Recherche de la valeur : ${value}` );
+	// on sauvegarde le texte recherché en LocalStorage
+	localStorage.setItem( 'lastSearch', value );
+	// on lance la requête AJAX vers l'API tvmaze
+	search( value );
+	// on envoie la recherche au serveur socket.io
+	liveSearch.emitSearch( value );
+}
+
+// Page de résultats
+const searchResults = new SearchResults();
+searchResults.results = [];
+const searchResultsContainer = $( '.searchResults' );
+searchResultsContainer.html( searchResults.render() );
+searchResults.mount( searchResultsContainer );
+
+
+// Chargement AJAX news.html
+function displayNews( html ) {
+	$( '.news' ).html( html );
+}
+fetch( './news.html' )
+	.then( response => response.text() )
+	.then( displayNews );
+
+// Chargement AJAX des résultats de recherche
+function search( value ){
+	// on met à jour le formulaire de recherche
+	// (utile si l'on vient de restaurer la recherche depuis le localStorage)
+	searchForm.search = value;
+	// on lance l'appel AJAX
+	fetch( `http://api.tvmaze.com/search/shows?q=${encodeURIComponent( value )}` )
+		.then( response => response.json() )
+		.then( data => {
+			// une fois les résultats reçus, on les envoie au searchResults
+			// pour les afficher
+			searchResults.results = data;
+		} );
+}
+
+// restauration de la recherche depuis le localStorage
+const lastSearch = localStorage.getItem('lastSearch');
+if ( lastSearch ) {
+	search( lastSearch );
+}
+
+// gestion de la liste des recherches
+const liveSearchContainer = $( '.liveSearch' );
+const liveSearch = new LiveSearch();
+liveSearch.onItemClick = search;
+liveSearchContainer.html( liveSearch.render() );
+liveSearch.mount( liveSearchContainer );
