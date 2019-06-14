cde9f7ecd85d33f1e51c9ed2afe9d34d3f02d507
thomas <tf@kumquats.fr>
2019-06-14 12:24:46 +0200

    8.1 demarrage : correction formatDate (heures & minutes)

--- 8.1-html5-video/demarrage/src/utils.js
+++ 8.1-html5-video/demarrage/src/utils.js
@@ -1,13 +1,13 @@
-// export const formatDate = date => date.replace(
-// 	/^(\d{2,4})-(\d{2})-(\d{2})$/,
-// 	'$3/$2/$1'
-// );
 export const formatDate = date => {
-	const monthNames = [ 'janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre' ];
-	const matches = date.match( /^(?<year>\d{2,4})-(?<month>\d{2})-(?<day>\d{2})$/ )
-	if ( !matches ) {
+	const monthNames = [ 'janvier','février','mars','avril','mai','juin','juillet','août','septembre','octobre','novembre','décembre'];
+	const matches = date.match(/^(?<year>\d{2,4})-(?<month>\d{2})-(?<day>\d{2})(T?(?<hours>\d{1,2}):(?<minutes>\d{1,2}).*)?$/);
+	if (!matches) {
 		return date;
 	}
-	const { year, month, day } = matches.groups;
-	return `${day} ${monthNames[ parseInt( month ) - 1 ]} ${year}`;
+	const {year, month, day, hours, minutes} = matches.groups;
+	let result = `${day} ${monthNames[parseInt(month) - 1]} ${year}`;
+	if ( hours && minutes ) {
+		result += ` à ${(parseInt(hours, 10) + 2)}h${ minutes }`;
+	}
+	return result;
 };
